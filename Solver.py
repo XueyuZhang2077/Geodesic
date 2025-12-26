fb7269d6ccea46e8769ab0646303c3cf2c97805f
XueyuZhang2077 <127658614+XueyuZhang2077@users.noreply.github.com>
2025-12-26 14:11:44 +0900

    Update Solver.py

--- Solver.py
+++ Solver.py
@@ -1,19 +1,71 @@
 import numpy as np
-import scipy.sparse as sp
+import scipy
+import sympy as sp
+
+
+
+
+class Metric:
+    def __init__(self,symbols_list,
+                      G_expr):
+        self._dim=len(symbols_list)
+
+        self._symbols=symbols_list
+
+        self._g=[ [self._expr_2_func(G_expr[i,j])  for j in range(i+1)] for i in range(self._dim)]
+
+        partialk_gij=[[ [ sp.diff(G_expr[i,j],symbols_list[k]) for j in range(i+1) ] for i in range(self._dim)]  for k in range(self._dim) ]
+
+        partiall_partialk_gij=[[[ [ sp.diff(partialk_gij[l][i][j],symbols_list[k]) for j in range(i+1) ] for i in range(self._dim)]  for k in range(l+1) ] for l in range(self._dim)]
+
+        self._dg=[[ [ self._expr_2_func(partialk_gij[k][i][j]) for j in range(i+1) ] for i in range(self._dim)]  for k in range(self._dim) ]
+
+        self._ddg=[[[ [ self._expr_2_func(partiall_partialk_gij[l][k][i][j]) for j in range(i+1) ] for i in range(self._dim)]  for k in range(l+1) ] for l in range(self._dim)]
+
+
+    def _expr_2_func(self,expr):
+        return sp.lambdify(self._symbols,expr,'numpy')
+
+    def G(self,coords):
+        cell_gi_gj=np.zeros((coords.shape[0],self._dim,self._dim),dtype=coords.dtype)
+        for i in range(self._dim):
+            for j in range(i+1):
+                cell_gi_gj[:,i,j]=self._g[i][j](*coords.T)
+                cell_gi_gj[:,j,i]=cell_gi_gj[:,i,j]
+        return cell_gi_gj
+    def dG(self,coords):
+        cell_partial_gi_gj=np.zeros((coords.shape[0],self._dim,self._dim,self._dim),dtype=coords.dtype)
+        for p in range(self._dim):
+            for i in range(self._dim):
+                for j in range(i+1):
+                    cell_partial_gi_gj[:,p,i,j]=self._dg[p][i][j](*coords.T)
+                    cell_partial_gi_gj[:,p,j,i]= cell_partial_gi_gj[:,p,i,j]
+        return cell_partial_gi_gj
+    
+    def ddG(self,coords):
+        cell_P_p_gi_gj=np.zeros((coords.shape[0],self._dim,self._dim,self._dim,self._dim),dtype=coords.dtype)
+        for P in range(self._dim):
+            for p in range(P+1):
+                for i in range(self._dim):
+                    for j in range(i+1):
+                        cell_P_p_gi_gj[:,P,p,i,j]=self._ddg[P][p][i][j](*coords.T)
+                        cell_P_p_gi_gj[:,P,p,j,i]=cell_P_p_gi_gj[:,P,p,i,j]
+                cell_P_p_gi_gj[:,p,P]=cell_P_p_gi_gj[:,P,p]
+        return cell_P_p_gi_gj
+    
 
 
 class Manifold:
     def __init__(self,
-                 metric,
-                 dmetric,
-                 ddmetric,
-                 dim,
-                 free_node_num_for_geodesic=100):
-        self._G=metric
-        self._dG=dmetric
-        self._ddG=ddmetric
-        self._dim=dim
-        self._test_func_num=free_node_num_for_geodesic*dim
+                 metric:Metric,
+                 divide_num=100
+                 ):
+        self._G=metric.G
+        self._dG=metric.dG
+        self._ddG=metric.ddG
+        self._dim=metric._dim
+        
+        self._divide_num=divide_num
         self._testfuncs_prep()
 
     def _connection_coef_info(self,x):
@@ -27,65 +79,52 @@ class Manifold:
         partial_gI=-np.einsum("...ik,...jl,...mkl->...mij",gI,gI,partial_g)
         temp_mlij=(np.einsum("...milj->...mlij",partial2_g)+np.einsum("...mjli->...mlij",partial2_g)-partial2_g)/2
         partial_m__Gammak_ij=np.einsum("...mkl,...lij->...mkij",partial_gI,Gamma_lij)+np.einsum("...kl,...mlij->...mkij",gI,temp_mlij)
-
-        return Gammak_ij,partial_m__Gammak_ij
+        
+        return Gammak_ij,np.append(partial_m__Gammak_ij,partial_m__Gammak_ij,axis=1)
 
     def _testfuncs_prep(self):
-        test_func_num,dim=self._test_func_num,self._dim
-        #row,col ids for test funcs
-        row=np.arange(0,test_func_num,1,dtype=int)
-        col=row.copy()
-        row=np.append(row,row)
-        col=np.append(col,col+dim)
 
-        self._delta_x=sp.coo_matrix((np.full(test_func_num*2,0.5),(row,col)),(test_func_num,test_func_num+dim))
-        self._delta_dx=sp.coo_matrix(
-            (np.append(np.ones(test_func_num),-np.ones(test_func_num)),(row,col)),
-            (test_func_num,test_func_num+dim)
-        )
-        self._delta_dx_dot_dx=self._delta_dx.dot(self._delta_dx.T)
-        print(self._delta_dx_dot_dx.toarray())
-        # self._delta_dx_Ddx=identitymatrix
+        self._delta_x=np.append(0.5*np.identity(self._dim,dtype=float),0.5*np.identity(self._dim,dtype=float),axis=0)
+        self._delta_dx=np.append(-np.identity(self._dim,dtype=float),np.identity(self._dim,dtype=float),axis=0)
 
-        #row,col ids for jacobi of Gamma^i_jk dx^j dx^k, etc.
-        row=np.array([np.full(dim,i) for i in range(test_func_num)])
-        col=np.einsum("...ij->...ji",row)
-        row,col=row.ravel(),col.ravel()
+        deltaID_in_cell0=np.arange(0,self._dim*2,1,dtype=int)
+        deltaID_partialID_in_cell0=np.array([[ [i,j] for j in range(self._dim*2)] for i in range(self._dim*2)])
+        cell_deltaID_partialID=np.array([deltaID_partialID_in_cell0+i*self._dim for i in range(self._divide_num)])
 
-        self._row=np.append(row,row+dim)
-        self._col=np.append(col,col)
-
+        self._cell_deltadx_partialdx=np.einsum("c,di,pi->cdp",np.ones(self._divide_num,dtype=float),self._delta_dx,self._delta_dx)
+        self._cell_delta_partialID_row=cell_deltaID_partialID[:,:,:,0].ravel()
+        self._cell_delta_partialID_col=cell_deltaID_partialID[:,:,:,1].ravel()
+        self._cell_deltaID=np.array([deltaID_in_cell0+i*self._dim for i in range(self._divide_num)]).ravel()
+        
 
     def solve_geodesic(self,ini_line_node_coords,tol=1e-8):
         line=ini_line_node_coords.copy()
-        free_shape=line[1:-1].shape
+        free_ids=np.arange(self._dim,self._divide_num*self._dim,1,dtype=int)
+        free2D_ids=np.ix_(free_ids,free_ids)
 
-        x=(line[:-1]+line[1:])/2.0
-        dx=line[1:]-line[:-1]
-        Gamma, partial_Gamma=self._connection_coef_info(x)
-        K,M=2*np.einsum("...ijk,...k->...ij",Gamma,dx),np.einsum("...lijk,...j,...k->...il",partial_Gamma,dx,dx)/2
-        v=np.einsum("...ij,...j->...i",K/2,dx)
-        err=self._delta_dx.dot(dx.ravel())-self._delta_x.dot(v.ravel())
-        print("err=",np.linalg.norm(err))
+        cell_x=(line[:-1]+line[1:])/2.0
+        cell_dx=line[1:]-line[:-1]
+        cell_Gammak_i_j, cell_partial_Gammak_i_j=self._connection_coef_info(cell_x)
+        cell_delta=np.einsum("ckij,ci,cj,dk->cd",cell_Gammak_i_j,cell_dx,cell_dx,self._delta_x)-np.einsum("di,ci->cd",self._delta_dx,cell_dx)
+        delta=np.zeros(self._dim*(self._divide_num+1),dtype=float)
+        np.add.at(delta,self._cell_deltaID,cell_delta.ravel())
+        err=np.linalg.norm(delta[free_ids])
+        print("err=",err)
         while np.linalg.norm(err)>tol:
+            cell_delta_partial=2*np.einsum("ckij,ci,pj,dk->cdp",cell_Gammak_i_j,cell_dx,self._delta_dx,self._delta_x)+np.einsum("cpkij,ci,cj,dk->cdp",cell_partial_Gammak_i_j,cell_dx,cell_dx,self._delta_x)-self._cell_deltadx_partialdx
+            delta_partial=np.zeros((delta.shape[0],delta.shape[0]),dtype=float)
+            np.add.at(delta_partial,(self._cell_delta_partialID_row,self._cell_delta_partialID_col),cell_delta_partial.ravel())
+            line[1:-1]-=(np.linalg.solve(delta_partial[free2D_ids],delta[free_ids])).reshape(line[1:-1].shape)
 
-            K=sp.coo_matrix((np.append(K[:-1].ravel(),-K[1:].ravel()),(self._row,self._col)),(self._test_func_num+self._dim,self._test_func_num))
-            M=sp.coo_matrix((np.append(M[:-1].ravel(),M[1:].ravel()),(self._row,self._col)),(self._test_func_num+self._dim,self._test_func_num))
-            Jacob=(self._delta_dx_dot_dx-self._delta_x.dot(K+M)).toarray()
-            line[1:-1]=(line[1:-1].ravel()-np.linalg.inv(Jacob)@err).reshape(free_shape)
-
-            x = (line[:-1] + line[1:]) / 2
-            dx = line[1:] - line[:-1]
-            Gamma, partial_Gamma = self._connection_coef_info(x)
-            K, M = 2 * np.einsum("...ijk,...k->...ij", Gamma, dx), np.einsum("...lijk,...j,...k->...il", partial_Gamma,
-                                                                             dx, dx) / 2
-            v = np.einsum("...ij,...j->...i", K / 2, dx)
-            err = self._delta_dx.dot(dx.ravel()) + self._delta_x.dot(v.ravel())
-            print("err=", np.linalg.norm(err))
-
+            cell_x=(line[:-1]+line[1:])/2.0
+            cell_dx=line[1:]-line[:-1]
+            cell_Gammak_i_j, cell_partial_Gammak_i_j=self._connection_coef_info(cell_x)
+            cell_delta=np.einsum("ckij,ci,cj,dk->cd",cell_Gammak_i_j,cell_dx,cell_dx,self._delta_x)-np.einsum("di,ci->cd",self._delta_dx,cell_dx)
+            delta=np.zeros(self._dim*(self._divide_num+1),dtype=float)
+            np.add.at(delta,self._cell_deltaID,cell_delta.ravel())
+            err=np.linalg.norm(delta[free_ids])
+            print("err=",err)
         return line
-
-
 
 
 
@@ -96,27 +135,31 @@ class Manifold:
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
-    def G(coords):
-        r=coords[:,0]
-        g=np.zeros((coords.shape[0],2,2),dtype=float)
-        g[:,0,0]=1.0
-        g[:,1,1]=r**2
-        return g
-    def dG(coords):
-        r=coords[:,0]
-        dg=np.zeros((coords.shape[0],2,2,2),dtype=float)
 
-        dg[:,0,1,1]=2*r
-
-        return dg
-    def ddG(coords):
-        r=coords[:,0]
-        ddg=np.zeros((coords.shape[0],2,2,2,2),dtype=float)
-        ddg[:,0,0,1,1]=2
-        return ddg
-
-    manifold=Manifold(G,dG,ddG,2,50)
-    ini_line=np.c_[np.linspace(1.0,1.0,52),np.linspace(-np.pi/2,-np.pi/4,52)]
+    x,y=sp.symbols("x y",positive=True)
+    r=sp.sqrt(x**2+y**2)
+    th=sp.atan(y/x)
+    p=sp.Matrix([r,th])
+    p_x=sp.diff(p,x)
+    p_y=sp.diff(p,y)
+    g_xx=sp.simplify(p_x.dot(p_x))
+    g_xy=sp.simplify(p_x.dot(p_y))
+    g_yy=sp.simplify(p_y.dot(p_y))
+    g=sp.Matrix([[g_xx,g_xy],
+                 [g_xy,g_yy]])
+    
+    metric=Metric([x,y],g)
+    manifold=Manifold(metric,100)
+    ini_line=np.c_[np.linspace(1.0,0.0,101),np.linspace(0.0,1.0,101)]
     line=manifold.solve_geodesic(ini_line)
-    plt.plot(line[:,0]*np.cos(line[:,1]),line[:,0]*np.sin(line[:,1]))
+    fig=plt.figure()
+    ax=fig.add_subplot(121)
+    ax.set(xlim=[-1.1,1.1],ylim=[-1.1,1.1],aspect=1)
+    ax.plot(np.sqrt(line[:,0]**2+line[:,1]**2),np.arctan(line[:,1]/line[:,0])) #in real manifold it is a straight line
+    ax.set_title("real manifold")
+
+    bx=fig.add_subplot(122)
+    bx.set(xlim=[-1.1,1.1],ylim=[-1.1,1.1],aspect=1)
+    bx.plot(line[:,0],line[:,1]) #but in the chart (or map) (x,y), it appears like a curved line
+    bx.set_title("map for the manifold")
     plt.show()
